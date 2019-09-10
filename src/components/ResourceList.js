f1d7f5028586cef90494ee89d0700eb63c163f51
Enzo Esperguin <enzoesper@hotmail.com>
2019-09-10 19:06:32 -0300

    refactoring
    extracting all the hook related stuff into a separate function

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,16 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const ResourceList = ({ resource }) => {
+const useResources = resource => {
   const [resources, setResource] = useState([]);
-
-  // const fetchResource = async resource => {
-  //   const response = await axios.get(
-  //     `https://jsonplaceholder.typicode.com/${resource}`
-  //   );
-
-  //   setResource(response.data);
-  // };
 
   useEffect(() => {
     (async resource => {
@@ -21,6 +13,12 @@ const ResourceList = ({ resource }) => {
       setResource(response.data);
     })(resource);
   }, [resource]);
+
+  return resources;
+};
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
 
   return (
     <ul>
